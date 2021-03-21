78feb54f4b3e274dd8a50ba3d06da2d53eee1a54
Komarana <komar.ana@ukr.net>
2021-03-21 23:08:49 +0300

    yyyyy

--- src/com/ua/lemon/lesson1/Number1_2.java
+++ src/com/ua/lemon/lesson1/Number1_2.java
@@ -1,4 +1,24 @@
 package com.ua.lemon.lesson1;
 
+import java.util.Scanner;
+
 public class Number1_2 {
-}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hours: ");
+        int hour = in.nextInt();
+        System.out.println("Rate per hour: ");
+        int rate = in.nextInt();
+
+        double allSalary = rate * hour;
+        double firstTax = allSalary * 0.05;
+        double secondTax = 1300;
+        double Salary = allSalary - firstTax - secondTax;
+
+        System.out.printf("All Salary: %.2f \nFirst Tax: %.2f \nSecond tax: %.2f \nFinal Salary: %.2f",
+                allSalary, firstTax, secondTax, Salary);
+
+    }
+    }
+
+
